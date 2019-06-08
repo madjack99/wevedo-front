5dfd5b5e8f4c9639dd59fafc609666fc583d2643
juan gaimaro <juangaimaro@ideesconsultora.com>
2019-06-08 13:40:50 -0300

    still working on home and stuff

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import './sass/wevedo.scss';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import MainPage from './components/MainPage';
+import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Header />
-        <MainPage />
+        <Home />
         <Footer />
       </React.Fragment>
     );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,83 +1,34 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import icons from '../../images/social-networks.png';
 
 class Footer extends Component {
-    state = {}
-
-    render() {
-      return (
-        <FooterWrapper>
-          <Row>
-            <ul>
-              <li className="footer-elements">
-                <Link path="/">
-                                2019 Wewedo
-                </Link>
-              </li>
-              <div className="footer-dot" />
-              <li className="footer-elements">
-                <Link path="/">
-                                Contact
-                </Link>
-              </li>
-              <div className="footer-dot" />
-              <li className="footer-elements">
-                <Link path="/">
-                  {'Terms & Conditions'}
-                </Link>
-              </li>
-              <div className="footer-dot" />
-              <li className="footer-elements">
-                <Link path="/">
-                                Privacy Policy
-                </Link>
-              </li>
-            </ul>
-            <img className="display-right" src={icons} alt="logo" />
+  render() {
+    return (
+      <React.Fragment>
+        <div className="footer-border"></div>
+        <Container className="footer">
+          <Row className="h-100 align-items-center">
+            <Col sm={10}>
+              <Nav className="text-uppercase">
+                <Nav.Link href="">© 2019 Wewedo</Nav.Link>
+                <div className="footer-dot"></div>
+                <Nav.Link href="">Contact</Nav.Link>
+                <div className="footer-dot"></div>
+                <Nav.Link href="">Terms & Conditions</Nav.Link>
+                <div className="footer-dot"></div>
+                <Nav.Link href="">Privacy Policy</Nav.Link>
+              </Nav>            
+            </Col>
+            <Col sm={2}>
+              <img className="display-right" src={icons} alt="logo" />
+            </Col>
           </Row>
-        </FooterWrapper>
-      );
-    }
+        </Container>
+      </React.Fragment>
+    );
+  }
 }
-
-const FooterWrapper = styled.footer`
-    height: 80px;
-    background-color: #ffffff;
-    border-top: 1px ridge;
-        
-    img {
-        height: 16px;
-        margin-top:38px;
-    }
-            
-.footer-elements{
-    font-size: 12px;
-    letter-spacing: 1.2px;
-}
-ul {    
-    display: flex;
-
-}
-li {
-    margin-top: 36px;
-    display:block;
-    text-transform: uppercase;
-}
-a {
-    text-decoration: none;
-    color: #898989;
-}
-.footer-dot {
-    width: 4px;
-    height: 4px;
-    background-image: linear-gradient(60deg, #ffafbd, #ffc3a0);
-    margin-left : 20px;
-    margin-right: 20px;
-    margin-top: 42px;
-    border-radius : 50%;
-}`;
 
 export default Footer;
