4eb1a9d08f57f24b3746f500635f5886140c47ce
juan gaimaro <juangaimaro@ideesconsultora.com>
2019-06-14 09:55:47 -0300

    login page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Contact from './components/Sections/Contact';
 import Terms from './components/Sections/Terms';
 import Privacy from './components/Sections/Privacy';
 import Login from './components/Login';
+import Signup from './components/Signup';
 
 const RouteMainLayout = ({ component: Component, ...rest }) => {
   return (
@@ -36,6 +37,7 @@ class App extends Component {
         <RouteMainLayout path="/terms" component={Terms} />
         <RouteMainLayout path="/privacy" component={Privacy} />
         <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
       </React.Fragment>
     );
   }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'react-bootstrap';
+import '../sass/wevedo.scss';
 
 class Login extends Component {
     render() {
-      return <h2>Login page</h2>
+      return (
+      	<div className="login">
+      		<Row className="h-100">
+      			<Col sm={6} className="login-img">
+      				
+      			</Col>
+      			<Col sm={6}>
+      				
+      			</Col>
+      		</Row>
+      	</div>
+			)
     }
 }
 
