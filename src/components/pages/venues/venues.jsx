c9dc57c6f01d90b13800fcbf219bcf4b50f9d0b9
RukkiesMan <pavelekseev@gmail.com>
2019-07-07 01:50:52 +0300

    Add pagination for the Venues page

--- src/components/pages/venues/venues.jsx
+++ src/components/pages/venues/venues.jsx
@@ -1,36 +1,95 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import {
   Row, Container, Col, Form, Button, ButtonToolbar,
 } from 'react-bootstrap';
+import Pagination from 'react-js-pagination';
 
 import './venues.scss';
 
 import 'rc-slider/assets/index.css';
 import { Range } from 'rc-slider';
 
-import sampleImg from '../../../assets/images/sample-list-img.png';
+import { WevedoServiceContext } from '../../contexts';
+
 import serches1 from '../../../assets/images/serches1.png';
 import serches2 from '../../../assets/images/serches2.png';
 import serches3 from '../../../assets/images/serches3.png';
 import serches4 from '../../../assets/images/serches4.png';
 
-const Venues = () => (
-  <React.Fragment>
+function Venues({ history, location }) {
+  const [providers, setProviders] = useState([]);
+  const [numberOfProviders, setNumberOfProviders] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const wevedoService = useContext(WevedoServiceContext);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const { data: newProviders } = await wevedoService.getListByCategory('Venue', currentPage);
+      const { data: newNumberOfProviders } = await wevedoService.getNumberOfProvidersByCategory('Venue');
+      setProviders(newProviders);
+      setNumberOfProviders(newNumberOfProviders);
+    };
+    fetchProviders();
+  }, [wevedoService, currentPage]);
+
+  useEffect(() => {
+    const match = location.search.match(/^\?page=(\d*)$/);
+    setCurrentPage(match ? match[1] : 1);
+  }, [location.pathname]);
+
+  const onPaginationChange = pageNumber => {
+    setCurrentPage(pageNumber);
+    history.push(`/venues?page=${pageNumber}`);
+    window.scrollTo(0, 0);
+  };
+
+  return (
+    <React.Fragment>
+      <Hero />
+      <SearchForm />
+      <Container className="venues-results">
+        <Row>
+          <Col sm={4} className="results-filters">
+            <Filters />
+          </Col>
+          <Col sm={8} className="results-data">
+            <Providers
+              providers={providers}
+              currentPage={currentPage}
+              numberOfProviders={numberOfProviders}
+              onPaginationChange={onPaginationChange}
+            />
+          </Col>
+        </Row>
+      </Container>
+      <PopularSearches />
+    </React.Fragment>
+  );
+}
+
+function Hero() {
+  return (
     <div className="section section-header-full venues">
       <Container className="h-100 w-100 align-items-center">
         <Row className="h-100 align-items-center">
           <Col sm={12} className="text-center text-uppercase">
             <h1>
-Wedding
+              Wedding
               <br />
               {' '}
-Venues
+              Venues
             </h1>
           </Col>
         </Row>
       </Container>
     </div>
+  );
+}
+
+function SearchForm() {
+  return (
     <Row className="findseparator findseparator-sm d-flex align-items-center text-center">
       <Col sm={12}>
         <Form>
@@ -56,136 +115,136 @@ Venues
         </Form>
       </Col>
     </Row>
-    <Container className="venues-results">
+  );
+}
+
+function Filters() {
+  return (
+    <React.Fragment>
+      <Form>
+        <div className="mb-5">
+          <div className="mb-3"><b>Budget</b></div>
+          <Row>
+            <Col sm={12}><Range min={0} max={20} defaultValue={[3, 10]} /></Col>
+            <Col sm={12} className="mt-3">
+              <span className="text-muted">Price: </span>
+              $30 - $100
+            </Col>
+          </Row>
+        </div>
+        <div className="mb-5">
+          <div className="mb-3"><b>Seated Dining Capacity</b></div>
+          <Row>
+            <Col sm={12}><Range min={0} max={20} defaultValue={[3, 10]} /></Col>
+            <Col sm={12} className="mt-3">
+              <span className="text-muted">Seated: </span>
+              30 - 100
+            </Col>
+          </Row>
+        </div>
+        <div className="mb-5">
+          <div className="mb-3"><b>Venue Type</b></div>
+          <Form.Check label="Country House" />
+          <Form.Check label="Barm" />
+          <Form.Check label="Outdoor" />
+          <Form.Check label="Attraction" />
+        </div>
+        <div className="mb-5">
+          <div className="mb-3"><b>Venue Styles</b></div>
+          <Form.Check label="Classic" />
+          <Form.Check label="Intimate" />
+          <Form.Check label="Unusual" />
+          <Form.Check label="Modern" />
+        </div>
+      </Form>
+      <ButtonToolbar>
+        <Button variant="primary" className="mr-2">Apply Filter</Button>
+        <Button variant="dark">Clear</Button>
+      </ButtonToolbar>
+    </React.Fragment>
+  );
+}
+
+function Providers({
+  providers, numberOfProviders, currentPage, onPaginationChange,
+}) {
+  function ProviderCard({ provider }) {
+    return (
+      <Link to="/supplier">
+        <Row>
+          <Col sm={5}><img src={provider.profileImageURL} alt="" /></Col>
+          <Col sm={7}>
+            <h5>{provider.firstName}</h5>
+            <span className="results-data-location">
+              <i className="fas fa-map-marker-alt" />
+              {' '}
+              {provider.regionName}
+            </span>
+            <p className="mt-2 mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...</p>
+            <b>$985 - $85,000 | Up to 220 Capacity</b>
+          </Col>
+        </Row>
+      </Link>
+    );
+  }
+
+  function PaginationButtons() {
+    return (
       <Row>
-        <Col sm={4} className="results-filters">
-          <Form>
-            <div className="mb-5">
-              <div className="mb-3"><b>Budget</b></div>
-              <Row>
-                <Col sm={12}><Range min={0} max={20} defaultValue={[3, 10]} /></Col>
-                <Col sm={12} className="mt-3">
-                  <span className="text-muted">Price: </span>
-                  $30 - $100
-                </Col>
-              </Row>
-            </div>
-            <div className="mb-5">
-              <div className="mb-3"><b>Seated Dining Capacity</b></div>
-              <Row>
-                <Col sm={12}><Range min={0} max={20} defaultValue={[3, 10]} /></Col>
-                <Col sm={12} className="mt-3">
-                  <span className="text-muted">Seated: </span>
-                  30 - 100
-                </Col>
-              </Row>
-            </div>
-            <div className="mb-5">
-              <div className="mb-3"><b>Venue Type</b></div>
-              <Form.Check label="Country House" />
-              <Form.Check label="Barm" />
-              <Form.Check label="Outdoor" />
-              <Form.Check label="Attraction" />
-            </div>
-            <div className="mb-5">
-              <div className="mb-3"><b>Venue Styles</b></div>
-              <Form.Check label="Classic" />
-              <Form.Check label="Intimate" />
-              <Form.Check label="Unusual" />
-              <Form.Check label="Modern" />
-            </div>
-          </Form>
+        <Col>
           <ButtonToolbar>
-            <Button variant="primary" className="mr-2">Apply Filter</Button>
-            <Button variant="dark">Clear</Button>
+            <Pagination
+              firstPageText={<i className="fa fa-angle-double-left" />}
+              prevPageText={<i className="fa fa-angle-left" />}
+              nextPageText={<i className="fa fa-angle-right" />}
+              lastPageText={<i className="fa fa-angle-double-right" />}
+              pageRangeDisplayed={3}
+              activePage={currentPage}
+              totalItemsCount={numberOfProviders}
+              onChange={onPaginationChange}
+              itemClass="paginationItem"
+              itemClassLast="paginationIcon"
+              itemClassNext="paginationIcon"
+              itemClassPrev="paginationIcon"
+              itemClassFirst="paginationIcon"
+            />
           </ButtonToolbar>
         </Col>
-        <Col sm={8} className="results-data">
-          <Col>
-            <Row className="mb-4">
-              <Col className="mr-auto text-uppercase"><h4 className="pt-2">740 Wedding Venues</h4></Col>
-              <Col className="text-right">
-                <Button variant="secondary" className="mr-2">Show map</Button>
-                <Button variant="secondary" className="mr-2"><i className="fas fa-th-large" /></Button>
-                <Button variant="primary"><i className="fas fa-bars" /></Button>
-              </Col>
-            </Row>
-            <Link to="/supplier">
-              <Row>
-                <Col sm={5}><img src={sampleImg} alt="" /></Col>
-                <Col sm={7}>
-                  <h5>Fulham Palace</h5>
-                  <span className="results-data-location">
-                    <i className="fas fa-map-marker-alt" />
-                    {' '}
-South London
-                  </span>
-                  <p className="mt-2 mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...</p>
-                  <b>$985 - $85,000 | Up to 220 Capacity</b>
-                </Col>
-              </Row>
-            </Link>
-            <div className="divider" />
-            <Row>
-              <Col sm={5}><img src={sampleImg} alt="" /></Col>
-              <Col sm={7}>
-                <h5>Fulham Palace</h5>
-                <span className="results-data-location">
-                  <i className="fas fa-map-marker-alt" />
-                  {' '}
-South London
-                </span>
-                <p className="mt-2 mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...</p>
-                <b>$985 - $85,000 | Up to 220 Capacity</b>
-              </Col>
-            </Row>
-            <div className="divider" />
-            <Row>
-              <Col sm={5}><img src={sampleImg} alt="" /></Col>
-              <Col sm={7}>
-                <h5>Fulham Palace</h5>
-                <span className="results-data-location">
-                  <i className="fas fa-map-marker-alt" />
-                  {' '}
-South London
-                </span>
-                <p className="mt-2 mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...</p>
-                <b>$985 - $85,000 | Up to 220 Capacity</b>
-              </Col>
-            </Row>
-            <div className="divider" />
-            <Row>
-              <Col sm={5}><img src={sampleImg} alt="" /></Col>
-              <Col sm={7}>
-                <h5>Fulham Palace</h5>
-                <span className="results-data-location">
-                  <i className="fas fa-map-marker-alt" />
-                  {' '}
-South London
-                </span>
-                <p className="mt-2 mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...</p>
-                <b>$985 - $85,000 | Up to 220 Capacity</b>
-              </Col>
-            </Row>
-            <Row className="mt-5">
-              <Col>
-                <ButtonToolbar>
-                  <Button variant="light" className="mr-2"><i className="fa fa-angle-double-left" /></Button>
-                  <Button variant="light" className="mr-2"><i className="fa fa-angle-left" /></Button>
-                  <Button className="mr-2">1</Button>
-                  <Button className="mr-2" variant="secondary">2</Button>
-                  <Button className="mr-2" variant="secondary">3</Button>
-                  <Button variant="light" className="mr-2"><i className="fa fa-angle-right" /></Button>
-                </ButtonToolbar>
-              </Col>
-              <Col className="text-right pt-2">
-                <span className="text-muted">Showing 1 - 6 of 8 results</span>
-              </Col>
-            </Row>
-          </Col>
+        <Col className="text-right pt-2">
+          <span className="text-muted">Showing 1 - 6 of 8 results</span>
         </Col>
       </Row>
-    </Container>
+    );
+  }
+
+  return (
+    <React.Fragment>
+      <Row className="mb-4">
+        <Col className="mr-auto text-uppercase"><h4 className="pt-2">740 Wedding Venues</h4></Col>
+        <Col className="text-right">
+          <Button variant="secondary" className="mr-2">Show map</Button>
+          <Button variant="secondary" className="mr-2"><i className="fas fa-th-large" /></Button>
+          <Button variant="primary"><i className="fas fa-bars" /></Button>
+        </Col>
+      </Row>
+      {
+        providers.map(provider => {
+          const { _id: id } = provider;
+          return (
+            <React.Fragment key={id}>
+              <ProviderCard provider={provider} />
+              <div className="divider" />
+            </React.Fragment>
+          );
+        })
+      }
+      <PaginationButtons className="mt-5" />
+    </React.Fragment>
+  );
+}
+
+function PopularSearches() {
+  return (
     <div className="popularserches">
       <Container className="pb-5">
         <Row>
@@ -253,7 +312,7 @@ South London
         </Col>
       </Row>
     </div>
-  </React.Fragment>
-);
+  );
+}
 
 export default Venues;
