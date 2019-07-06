c08e55753f144e409fa5d84f4fbfc2cbc095ab88
RukkiesMan <pavelekseev@gmail.com>
2019-07-06 23:41:43 +0300

    Add methods to get categories data

--- src/api/services/wevedo-service.js
+++ src/api/services/wevedo-service.js
@@ -9,5 +9,10 @@ export default class WevedoService extends ApiBase {
     this.signOut = async () => this.get('signout');
 
     this.getCategories = () => this.get('categories');
+
+    this.getListByCategory = (category, page = 1) => this.get(
+      `providers/categories/${category}?page=${page}`,
+    );
+    this.getNumberOfProvidersByCategory = category => this.get(`providers/number/${category}`);
   }
 }
