fd33a4808dd54395dd490ccac748f75dbf80c910
madjack99 <madjack@nm.ru>
2019-07-08 15:04:51 +0400

    convert contact.jsx to class component, add state

--- src/components/pages/contact/contact.jsx
+++ src/components/pages/contact/contact.jsx
@@ -1,130 +1,147 @@
-import React from 'react';
-import {
-  Row, Container, Col, Form, Button,
-} from 'react-bootstrap';
+import React from "react";
+import { Row, Container, Col, Form, Button } from "react-bootstrap";
 
-import './contact.scss';
+import "./contact.scss";
 
-import serches1 from '../../../assets/images/serches1.png';
-import serches2 from '../../../assets/images/serches2.png';
-import serches3 from '../../../assets/images/serches3.png';
-import serches4 from '../../../assets/images/serches4.png';
+import serches1 from "../../../assets/images/serches1.png";
+import serches2 from "../../../assets/images/serches2.png";
+import serches3 from "../../../assets/images/serches3.png";
+import serches4 from "../../../assets/images/serches4.png";
 
-const Contact = () => (
-  <React.Fragment>
-    <div className="section section-header-half contact">
-      <Container className="h-100 w-100 align-items-center">
-        <Row className="h-100 align-items-center">
-          <Col sm={12} className="text-center text-uppercase">
-            <h1>Contact Us</h1>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
 
-    <Container className="mt-5 mb-5">
-      <Row className="justify-content-center mt-5 mb-5">
-        <Col sm={8} className="text-center">
-          <h4 className="text-uppercase">Contact to wevedo</h4>
-          <hr />
-          <p className="mr-5 pr-5 ml-5 pl-5 text-muted">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-          <Form className="mt-5">
-            <Row>
-              <Col sm={6}>
-                <Form.Group controlId="">
-                  <Form.Control type="text" placeholder="Name" />
-                </Form.Group>
-              </Col>
-              <Col sm={6}>
-                <Form.Group controlId="">
-                  <Form.Control type="email" placeholder="Email" />
-                </Form.Group>
-              </Col>
-              <Col sm={12} className="mt-5">
-                <Form.Group controlId="">
-                  <Form.Control as="textarea" placeholder="Message" rows="3" />
-                </Form.Group>
-              </Col>
-              <Col className="text-center text-uppercase mt-5 mb-5">
-                <Button size="lg">Send</Button>
+    this.state = {
+      name: "",
+      email: "",
+      message: ""
+    };
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="section section-header-half contact">
+          <Container className="h-100 w-100 align-items-center">
+            <Row className="h-100 align-items-center">
+              <Col sm={12} className="text-center text-uppercase">
+                <h1>Contact Us</h1>
               </Col>
             </Row>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+          </Container>
+        </div>
 
-    <div className="popularserches">
-      <Container className="pb-5">
-        <Row>
-          <Col sm={6}>
-            <h3 className="text-uppercase">Popular venue serches</h3>
-            <p className="d-none d-sm-block">
-              At some stage in all our lives we want clearer, fresher, younger looking skin.
-              Well it can be achieved without spending a lot of money.
-            </p>
-          </Col>
-          <Col sm={6}>
+        <Container className="mt-5 mb-5">
+          <Row className="justify-content-center mt-5 mb-5">
+            <Col sm={8} className="text-center">
+              <h4 className="text-uppercase">Contact to wevedo</h4>
+              <hr />
+              <p className="mr-5 pr-5 ml-5 pl-5 text-muted">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <Form className="mt-5">
+                <Row>
+                  <Col sm={6}>
+                    <Form.Group controlId="">
+                      <Form.Control type="text" placeholder="Name" />
+                    </Form.Group>
+                  </Col>
+                  <Col sm={6}>
+                    <Form.Group controlId="">
+                      <Form.Control type="email" placeholder="Email" />
+                    </Form.Group>
+                  </Col>
+                  <Col sm={12} className="mt-5">
+                    <Form.Group controlId="">
+                      <Form.Control
+                        as="textarea"
+                        placeholder="Message"
+                        rows="3"
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col className="text-center text-uppercase mt-5 mb-5">
+                    <Button size="lg">Send</Button>
+                  </Col>
+                </Row>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+
+        <div className="popularserches">
+          <Container className="pb-5">
             <Row>
-              <Col>
-                <ul>
-                  <li>Essex</li>
-                  <li>Hertforshire</li>
-                  <li>West Midlands</li>
-                  <li>Hampshire</li>
-                </ul>
+              <Col sm={6}>
+                <h3 className="text-uppercase">Popular venue serches</h3>
+                <p className="d-none d-sm-block">
+                  At some stage in all our lives we want clearer, fresher,
+                  younger looking skin. Well it can be achieved without spending
+                  a lot of money.
+                </p>
               </Col>
-              <Col>
-                <ul>
-                  <li>Essex</li>
-                  <li>Hertforshire</li>
-                  <li>West Midlands</li>
-                  <li>Hampshire</li>
-                </ul>
-              </Col>
-              <Col className="d-none d-sm-block">
-                <ul>
-                  <li>Essex</li>
-                  <li>Hertforshire</li>
-                  <li>West Midlands</li>
-                  <li>Hampshire</li>
-                </ul>
+              <Col sm={6}>
+                <Row>
+                  <Col>
+                    <ul>
+                      <li>Essex</li>
+                      <li>Hertforshire</li>
+                      <li>West Midlands</li>
+                      <li>Hampshire</li>
+                    </ul>
+                  </Col>
+                  <Col>
+                    <ul>
+                      <li>Essex</li>
+                      <li>Hertforshire</li>
+                      <li>West Midlands</li>
+                      <li>Hampshire</li>
+                    </ul>
+                  </Col>
+                  <Col className="d-none d-sm-block">
+                    <ul>
+                      <li>Essex</li>
+                      <li>Hertforshire</li>
+                      <li>West Midlands</li>
+                      <li>Hampshire</li>
+                    </ul>
+                  </Col>
+                </Row>
               </Col>
             </Row>
-          </Col>
-        </Row>
-      </Container>
-      <Row className="m-0">
-        <Col xs={3} className="p-0 overlayed">
-          <img src={serches1} alt="" />
-          <div className="overlay">
-            <i className="fa fa-search fa-2x" />
-          </div>
-        </Col>
-        <Col xs={3} className="p-0 overlayed">
-          <img src={serches2} alt="" />
-          <div className="overlay">
-            <i className="fa fa-search fa-2x" />
-          </div>
-        </Col>
-        <Col xs={3} className="p-0 overlayed">
-          <img src={serches3} alt="" />
-          <div className="overlay">
-            <i className="fa fa-search fa-2x" />
-          </div>
-        </Col>
-        <Col xs={3} className="p-0 overlayed">
-          <img src={serches4} alt="" />
-          <div className="overlay">
-            <i className="fa fa-search fa-2x" />
-          </div>
-        </Col>
-      </Row>
-    </div>
-  </React.Fragment>
-);
+          </Container>
+          <Row className="m-0">
+            <Col xs={3} className="p-0 overlayed">
+              <img src={serches1} alt="" />
+              <div className="overlay">
+                <i className="fa fa-search fa-2x" />
+              </div>
+            </Col>
+            <Col xs={3} className="p-0 overlayed">
+              <img src={serches2} alt="" />
+              <div className="overlay">
+                <i className="fa fa-search fa-2x" />
+              </div>
+            </Col>
+            <Col xs={3} className="p-0 overlayed">
+              <img src={serches3} alt="" />
+              <div className="overlay">
+                <i className="fa fa-search fa-2x" />
+              </div>
+            </Col>
+            <Col xs={3} className="p-0 overlayed">
+              <img src={serches4} alt="" />
+              <div className="overlay">
+                <i className="fa fa-search fa-2x" />
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 
 export default Contact;
