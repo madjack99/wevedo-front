ab1a6605095d272f4ec7176e8feafb643b821fde
madjack99 <alex.a@evada.org>
2019-07-14 15:57:22 +0400

    add i18nProvder and i18n.init to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
 
 import App from './components/app';
 import WevedoService from './api/services/wevedo-service';
@@ -10,12 +12,19 @@ import store from './store';
 
 const wevedoService = new WevedoService();
 
-ReactDOM.render((
-  <Provider store={store}>
-    <WevedoServiceContext.Provider value={wevedoService}>
-      <Router>
-        <App />
-      </Router>
-    </WevedoServiceContext.Provider>
-  </Provider>
-), document.getElementById('root'));
+i18next.init({
+  interpolation: { escapeValue: false },
+});
+
+ReactDOM.render(
+  <I18nextProvider i18n={i18next}>
+    <Provider store={store}>
+      <WevedoServiceContext.Provider value={wevedoService}>
+        <Router>
+          <App />
+        </Router>
+      </WevedoServiceContext.Provider>
+    </Provider>
+  </I18nextProvider>,
+  document.getElementById('root'),
+);
