cfd8057777fa00695a711f604afc9f37b09f95a6
madjack99 <alex.a@evada.org>
2019-07-14 19:57:59 +0400

    add withTranslation to privacy

--- src/components/pages/privacy/privacy.jsx
+++ src/components/pages/privacy/privacy.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Row, Container, Col } from 'react-bootstrap';
+import { withTranslation } from 'react-i18next';
 
 import PopularSearches from '../popularSearches';
 import './privacy.scss';
 
-const Privacy = () => (
+const Privacy = ({ t }) => (
   <React.Fragment>
     <div className="section section-header-half privacy">
       <Container className="h-100 w-100 align-items-center">
         <Row className="h-100 align-items-center">
           <Col sm={12} className="text-center text-uppercase">
-            <h1>Privacy Policy</h1>
+            <h1>{t('privacy.jumbotron')}</h1>
           </Col>
         </Row>
       </Container>
@@ -19,51 +20,21 @@ const Privacy = () => (
     <Container className="mt-5 mb-5">
       <Row className="justify-content-center mt-5 mb-5">
         <Col sm={8} className="text-center mb-3">
-          <h4 className="text-uppercase">Data protection policy</h4>
+          <h4 className="text-uppercase">{t('privacy.mainTitle')}</h4>
           <hr />
         </Col>
       </Row>
       <Row>
         <Col sm={12} className="mb-5">
-          <h5 className="text-uppercase text-proxima-bold">1. Controller</h5>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
-          </p>
-          <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-            laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-            architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
-            sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-            voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
-            amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut
-            labore et dolore magnam aliquam quaerat voluptatem.
-          </p>
+          <h5 className="text-uppercase text-proxima-bold">{t('privacy.firstText.titleOne')}</h5>
+          <p>{t('privacy.firstText.pOne')}</p>
+          <p>{t('privacy.firstText.pTwo')}</p>
         </Col>
         <Col sm={12} className="mb-5">
-          <h5 className="text-uppercase text-proxima-bold">2. Personal data</h5>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
-          </p>
+          <h5 className="text-uppercase text-proxima-bold">{t('privacy.secondText.titleOne')}</h5>
+          <p>{t('privacy.secondText.pOne')}</p>
 
-          <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-            laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-            architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
-            sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-            voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
-            amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut
-            labore et dolore magnam aliquam quaerat voluptatem.
-          </p>
+          <p>{t('privacy.secondText.pTwo')}</p>
         </Col>
       </Row>
     </Container>
@@ -71,4 +42,4 @@ const Privacy = () => (
   </React.Fragment>
 );
 
-export default Privacy;
+export default withTranslation('common')(Privacy);
