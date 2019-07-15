5e4377faaddb948ed354cb0ac2885b9067448f6b
madjack99 <alex.a@evada.org>
2019-07-15 15:54:15 +0400

    add withTranslation to terms-and-conditions

--- src/components/pages/terms-and-conditions/terms-and-conditions.jsx
+++ src/components/pages/terms-and-conditions/terms-and-conditions.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Row, Container, Col } from 'react-bootstrap';
+import { withTranslation } from 'react-i18next';
 
 import PopularSearches from '../popularSearches';
 import './terms-and-conditions.scss';
 
-const TermsAndConditions = () => (
+const TermsAndConditions = ({ t }) => (
   <React.Fragment>
     <div className="section section-header-half terms-and-conditions">
       <Container className="h-100 w-100 align-items-center">
         <Row className="h-100 align-items-center">
           <Col sm={12} className="text-center text-uppercase">
-            <h1>Terms & Conditions</h1>
+            <h1>{t('terms.jumbotron')}</h1>
           </Col>
         </Row>
       </Container>
@@ -19,52 +20,20 @@ const TermsAndConditions = () => (
     <Container className="mt-5 mb-5">
       <Row className="justify-content-center mt-5 mb-5">
         <Col sm={8} className="text-center mb-3">
-          <h4 className="text-uppercase">
-            Legal terms and conditions for users of the wevedo website
-          </h4>
+          <h4 className="text-uppercase">{t('terms.mainTitle')}</h4>
           <hr />
         </Col>
       </Row>
       <Row>
         <Col sm={12} className="mb-5">
-          <h5 className="text-uppercase text-proxima-bold">1. Legal Information</h5>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
-          </p>
-          <p>
-            Sed ut perspiciatis unde omnis iste natus error sitvoluptatem accusantium doloremque
-            laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-            architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
-            sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-            voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
-            amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut
-            labore et dolore magnam aliquam quaerat voluptatem.
-          </p>
+          <h5 className="text-uppercase text-proxima-bold">{t('terms.firstText.titleOne')}</h5>
+          <p>{t('terms.firstText.pOne')}</p>
+          <p>{t('terms.firstText.pTwo')}</p>
         </Col>
         <Col sm={12} className="mb-5">
-          <h5 className="text-uppercase text-proxima-bold">2. Purpose</h5>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
-          </p>
-          <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-            laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-            architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
-            sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-            voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
-            amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut
-            labore et dolore magnam aliquam quaerat voluptatem.
-          </p>
+          <h5 className="text-uppercase text-proxima-bold">{t('terms.secondText.titleOne')}</h5>
+          <p>{t('terms.secondText.pOne')}</p>
+          <p>{t('terms.secondText.pTwo')}</p>
         </Col>
       </Row>
     </Container>
@@ -73,4 +42,4 @@ const TermsAndConditions = () => (
   </React.Fragment>
 );
 
-export default TermsAndConditions;
+export default withTranslation('common')(TermsAndConditions);
