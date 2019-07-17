606ec9d94d644549ef1e88415aca5ede7595e559
RukkiesMan <pavelekseev@gmail.com>
2019-07-17 21:54:36 +0300

    Show updated enter screens by links

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -13,11 +13,11 @@ import Supplier from '../pages/supplier';
 import Contact from '../pages/contact';
 import Terms from '../pages/terms-and-conditions';
 import Privacy from '../pages/privacy';
-import Login from '../login';
-import Signup from '../sign-up';
 import Pricing from '../pricing';
 import BusinessLogin from '../business-login';
 import BusinessSignup from '../business-sign-up';
+import ScreensUserSignUp from '../../screens/user/sign-up';
+import ScreensUserLogin from '../../screens/user/login';
 
 const RouteMainLayout = ({ component: Component, ...rest }) => (
   <Route
@@ -44,8 +44,8 @@ const App = () => (
       <RouteMainLayout path="/contact" component={Contact} />
       <RouteMainLayout path="/terms" component={Terms} />
       <RouteMainLayout path="/privacy" component={Privacy} />
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={Signup} />
+      <Route path="/login" component={ScreensUserLogin} />
+      <Route path="/signup" component={ScreensUserSignUp} />
       <Route path="/pricing" component={Pricing} />
       <Route path="/business-login" component={BusinessLogin} />
       <Route path="/business-signup" component={BusinessSignup} />
