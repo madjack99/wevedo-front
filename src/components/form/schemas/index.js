8b14dd4f1930655eb97663507ac364398f37fe1e
RukkiesMan <pavelekseev@gmail.com>
2019-07-18 12:40:59 +0300

    Move form schemas to form folder in components

--- src/components/form/schemas/index.js
+++ src/components/form/schemas/index.js
@@ -3,17 +3,17 @@ import * as Yup from 'yup';
 export const signUpFormSchema = Yup.object().shape({
   email: Yup.string()
     .email()
-    .required('Email is required!'),
+    .required('email is required!'),
   password: Yup.string()
     .min(6)
-    .required('Password is required!'),
+    .required('password is required!'),
 });
 
 export const loginFormSchema = Yup.object().shape({
   email: Yup.string()
     .email()
-    .required('Email is required!'),
+    .required('email is required!'),
   password: Yup.string()
     .min(6)
-    .required('Password is required!'),
+    .required('password is required!'),
 });
