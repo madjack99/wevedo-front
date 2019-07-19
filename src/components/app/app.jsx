bc767f957c17fd6aefc241627cd23f8d5bd629e8
RukkiesMan <pavelekseev@gmail.com>
2019-07-19 15:02:24 +0300

    Remove old BusinessSignup component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -17,7 +17,6 @@ import Terms from '../pages/terms-and-conditions';
 import Privacy from '../pages/privacy';
 import Pricing from '../pricing';
 import BusinessLogin from '../business-login';
-import BusinessSignup from '../business-sign-up';
 import ImgUpload from '../imgUpload';
 import ScreensUserSignUp from '../../screens/user/sign-up';
 import ScreensUserLogin from '../../screens/user/login';
@@ -64,7 +63,6 @@ const App = ({ getCategories }) => {
         <Route path="/business-login" component={BusinessLogin} />
         <Route path="/business-signup-1" component={ScreensBusinessSignUpFirstStep} />
         <Route path="/businesssignup-step1" component={ImgUpload} />
-        // Надо уточнить какой путь будет вести к моему компоненту
       </Switch>
     </React.Fragment>
   );
