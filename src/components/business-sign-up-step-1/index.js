5fda0181c758a60b040d290cbe2cb39340481fb8
RukkiesMan <pavelekseev@gmail.com>
2019-07-23 10:43:33 +0300

    Remove unnecessary components

--- src/components/business-sign-up-step-1/index.js
+++
@@ -1,3 +0,0 @@
-import BusinessLogin from './business-sign-up-step-1';
-
-export default BusinessLogin;
--- src/components/business-sign-up-step-2/index.js
+++
@@ -1,3 +0,0 @@
-import BusinessLogin from './business-sign-up-step-2';
-
-export default BusinessLogin;
