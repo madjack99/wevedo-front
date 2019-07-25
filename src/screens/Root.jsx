0b7ed9b5e9febe1dad7d49d85e747097884abe2e
RukkiesMan <pavelekseev@gmail.com>
2019-07-25 15:52:36 +0300

    Add root file for screens

---
+++ src/screens/Root.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+
+const ScreensRoot = () => {
+  return (
+    <Router>
+      <Switch></Switch>
+    </Router>
+  );
+};
+
+export default ScreensRoot;
