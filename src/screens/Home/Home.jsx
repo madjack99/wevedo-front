02df78faa72c10bf9d41cdb3bf16f40105d5457e
RukkiesMan <pavelekseev@gmail.com>
2019-07-25 16:39:10 +0300

    Move popular searches to standalone component

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -18,10 +18,6 @@ import musicphoto from '../../assets/images/music.png';
 import florist from '../../assets/images/florist.png';
 import photography from '../../assets/images/photography.png';
 import decoration from '../../assets/images/decoration.png';
-import serches1 from '../../assets/images/serches1.png';
-import serches2 from '../../assets/images/serches2.png';
-import serches3 from '../../assets/images/serches3.png';
-import serches4 from '../../assets/images/serches4.png';
 
 import ScreensLayoutsMain from '../Layouts/Main';
 
@@ -113,77 +109,7 @@ const ScreensHome = ({ t }) => {
           </Row>
         </div>
       </Container>
-
       <SearchForm t={t} />
-
-      <div className="popularserches">
-        <Container className="pb-3 pb-sm-5">
-          <Row>
-            <Col sm={6}>
-              <h3 className="text-uppercase">Popular venue serches</h3>
-              <p className="d-none d-sm-block">
-                At some stage in all our lives we want clearer, fresher, younger
-                looking skin. Well it can be achieved without spending a lot of
-                money.
-              </p>
-            </Col>
-            <Col sm={6}>
-              <Row>
-                <Col>
-                  <ul>
-                    <li>Essex</li>
-                    <li>Hertforshire</li>
-                    <li>West Midlands</li>
-                    <li>Hampshire</li>
-                  </ul>
-                </Col>
-                <Col>
-                  <ul>
-                    <li>Essex</li>
-                    <li>Hertforshire</li>
-                    <li>West Midlands</li>
-                    <li>Hampshire</li>
-                  </ul>
-                </Col>
-                <Col className="d-none d-sm-block">
-                  <ul>
-                    <li>Essex</li>
-                    <li>Hertforshire</li>
-                    <li>West Midlands</li>
-                    <li>Hampshire</li>
-                  </ul>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        </Container>
-        <Row className="m-0">
-          <Col xs={4} sm={3} className="p-0 overlayed">
-            <img src={serches1} alt="" />
-            <div className="overlay">
-              <i className="fa fa-search fa-2x" />
-            </div>
-          </Col>
-          <Col xs={4} sm={3} className="p-0 overlayed">
-            <img src={serches2} alt="" />
-            <div className="overlay">
-              <i className="fa fa-search fa-2x" />
-            </div>
-          </Col>
-          <Col xs={4} sm={3} className="p-0 overlayed">
-            <img src={serches3} alt="" />
-            <div className="overlay">
-              <i className="fa fa-search fa-2x" />
-            </div>
-          </Col>
-          <Col sm={3} className="p-0 overlayed d-none d-sm-flex">
-            <img src={serches4} alt="" />
-            <div className="overlay">
-              <i className="fa fa-search fa-2x" />
-            </div>
-          </Col>
-        </Row>
-      </div>
     </ScreensLayoutsMain>
   );
 };
