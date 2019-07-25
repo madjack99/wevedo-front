78b1f9bd0ab8312a0a6cd0451b2f8a6ba7a5a11e
RukkiesMan <pavelekseev@gmail.com>
2019-07-25 16:39:33 +0300

    Add popular searches to main layout screen

--- src/screens/Layouts/Main/Main.jsx
+++ src/screens/Layouts/Main/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Header from '../../../components/Header';
+import PopularSearches from '../../../components/PopularSearches';
 import Footer from '../../../components/Footer';
 
 const ScreensLayoutsMain = ({ children }) => (
   <React.Fragment>
     <Header />
     {children}
+    <PopularSearches />
     <Footer />
   </React.Fragment>
 );
