b77f7575c986ff7e1593bced34c95f29a5596571
RukkiesMan <pavelekseev@gmail.com>
2019-07-25 17:33:18 +0300

    Update privacy policy screen

--- src/screens/Root.jsx
+++ src/screens/Root.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import ScreensHome from './Home';
+import ScreensPrivacyPolicy from './PrivacyPolicy/PrivacyPolicy';
 
 const ScreensRoot = () => {
   return (
     <Router>
       <Switch>
         <Route path="/" component={ScreensHome} exact />
+        <Route path="/privacy-policy" component={ScreensPrivacyPolicy} />
       </Switch>
     </Router>
   );
