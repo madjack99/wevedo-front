fd08a0bcdcba5c701838c6ba36b8ad01bba06795
RukkiesMan <pavelekseev@gmail.com>
2019-07-25 15:54:08 +0300

    Update home screen

--- src/screens/Root.jsx
+++ src/screens/Root.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import ScreensHome from './Home';
 
 const ScreensRoot = () => {
   return (
     <Router>
-      <Switch></Switch>
+      <Switch>
+        <Route path="/" component={ScreensHome} exact />
+      </Switch>
     </Router>
   );
 };
