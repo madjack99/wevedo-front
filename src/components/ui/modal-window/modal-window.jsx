1f24e5a44c3053e092d6c72557d18af791c12b3d
RukkiesMan <pavelekseev@gmail.com>
2019-07-26 11:19:11 +0300

    Fix modal window styles

--- src/components/ui/modal-window/modal-window.jsx
+++ src/components/ui/modal-window/modal-window.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { Modal } from 'react-bootstrap';
 
-import './modal-window.scss';
-
 export default function ModalWindow({ show, onHide, children }) {
   return (
     <Modal show={show} onHide={onHide} className="global-modal" centered>
       <Modal.Header className="pb-0 border-0" closeButton />
-      <Modal.Body className="p-0">
-        {children}
-      </Modal.Body>
+      <Modal.Body className="p-0">{children}</Modal.Body>
     </Modal>
   );
 }
