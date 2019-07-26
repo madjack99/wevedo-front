6dce428a429d82acb4c64b69abdef6be9797035a
RukkiesMan <pavelekseev@gmail.com>
2019-07-26 10:14:34 +0300

    Update terms and conditions screen

--- src/screens/Root.jsx
+++ src/screens/Root.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import ScreensHome from './Home';
-import ScreensPrivacyPolicy from './PrivacyPolicy/PrivacyPolicy';
 import ScreensContact from './Contact';
+import ScreensTermsAndConditions from './TermsAndConditions';
+import ScreensPrivacyPolicy from './PrivacyPolicy/PrivacyPolicy';
 
 const ScreensRoot = () => {
   return (
     <Router>
       <Switch>
         <Route path="/" component={ScreensHome} exact />
-        <Route path="/privacy-policy" component={ScreensPrivacyPolicy} />
         <Route path="/contact" component={ScreensContact} />
+        <Route
+          path="/terms-and-conditions"
+          component={ScreensTermsAndConditions}
+        />
+        <Route path="/privacy-policy" component={ScreensPrivacyPolicy} />
       </Switch>
     </Router>
   );
