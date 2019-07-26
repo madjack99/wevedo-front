8364fffdcac65970da334890c8f1b2bd6d745aab
RukkiesMan <pavelekseev@gmail.com>
2019-07-26 11:18:17 +0300

    Rename enter to Enter layout

--- src/screens/Layouts/Enter/Enter.jsx
+++ src/screens/Layouts/Enter/Enter.jsx
@@ -3,27 +3,33 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
-import './enter.scss';
+import './Enter.scss';
 
 import logo from '../../../assets/images/symbol.png';
 
 const ScreensLayoutEnter = ({
-  sideBackground, welcomeTitle, welcomeSubtitle, welcomeText, children,
+  sideBackground,
+  welcomeTitle,
+  welcomeSubtitle,
+  welcomeText,
+  children,
 }) => {
   return (
     <Container className="enter-business-screen p-0" fluid>
       <Row className="w-100 m-0">
         <Col className="position-relative side-background p-0" md={6}>
-          <img className="side-background-image" src={sideBackground} alt="background" />
+          <img
+            className="side-background-image"
+            src={sideBackground}
+            alt="background"
+          />
           <div className="welcome-block">
             <h2 className="welcome-block__title">{welcomeTitle}</h2>
             <p className="welcome-block__subtitle">{welcomeSubtitle}</p>
             <div className="welcome-block__divider" />
-            {
-              welcomeText
-                ? <p className="welcome-block__text">{welcomeText}</p>
-                : null
-            }
+            {welcomeText ? (
+              <p className="welcome-block__text">{welcomeText}</p>
+            ) : null}
           </div>
         </Col>
         <Col className="enter-business-screen_right" md={6}>
