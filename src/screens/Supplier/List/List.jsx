a5e18e6d6b180401245a7a237fc36c5961dbf9e2
RukkiesMan <pavelekseev@gmail.com>
2019-07-27 10:01:33 +0300

    Update supplier list

--- src/screens/Supplier/List/List.jsx
+++ src/screens/Supplier/List/List.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import {
-  Row, Container, Col, Form, Button,
-} from 'react-bootstrap';
+import { Row, Container, Col } from 'react-bootstrap';
 
-import PopularSearches from '../popularSearches';
 import Beauty from '../../../assets/images/w-beauty.png';
 import Cakes from '../../../assets/images/w-cakes.png';
 import Decoration from '../../../assets/images/w-decoration.png';
@@ -11,49 +8,18 @@ import Florist from '../../../assets/images/w-florist.png';
 import Music from '../../../assets/images/w-music.png';
 import Rings from '../../../assets/images/w-rings.png';
 
+import backgroundImage from '../../../assets/images/wedding-suppliers-bg.png';
+
+import ScreensLayoutMain from '../../Layouts/Main';
+import SearchPanel from '../../../components/SearchPanel';
+
 export default function WeddingSuppliers() {
   return (
-    <React.Fragment>
-      <div className="section section-header-full wedding-suppliers">
-        <Container className="h-100 w-100 align-items-center">
-          <Row className="h-100 align-items-center">
-            <Col sm={12} className="text-center text-uppercase">
-              <h1>
-                Wedding
-                <br />
-                Suppliers
-              </h1>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-      <Row className="findseparator findseparator-sm d-flex align-items-center text-center">
-        <Col sm={12}>
-          <Form>
-            <Form.Row>
-              <Col className="boxed-form">
-                <Row>
-                  <Col>
-                    <Form.Control as="select">
-                      <option>Category</option>
-                      <option>...</option>
-                    </Form.Control>
-                  </Col>
-                  <div className="divider" />
-                  <Col>
-                    <Form.Control placeholder="Location" />
-                  </Col>
-                </Row>
-              </Col>
-              <Col sm={3}>
-                <Button variant="light" size="lg">
-                  Search
-                </Button>
-              </Col>
-            </Form.Row>
-          </Form>
-        </Col>
-      </Row>
+    <ScreensLayoutMain
+      title="Wedding Suppliers"
+      backgroundImage={backgroundImage}
+    >
+      <SearchPanel />
       <Container className="mt-5 mb-5 pt-5 pb-5 wedding-suppliers-grid">
         <Row className="mb-5">
           <Col>
@@ -88,7 +54,6 @@ export default function WeddingSuppliers() {
           </Col>
         </Row>
       </Container>
-      <PopularSearches />
-    </React.Fragment>
+    </ScreensLayoutMain>
   );
 }
