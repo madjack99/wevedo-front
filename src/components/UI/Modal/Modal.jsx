38c953783dd22b447b0be8ed36d258fb24232db0
RukkiesMan <pavelekseev@gmail.com>
2019-07-27 09:02:56 +0300

    Rename modal-window to Modal

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+
 import { Modal } from 'react-bootstrap';
 
-export default function ModalWindow({ show, onHide, children }) {
+export default function UIModal({ show, onHide, children }) {
   return (
-    <Modal show={show} onHide={onHide} className="global-modal" centered>
+    <Modal show={show} onHide={onHide} className="ui-modal" centered>
       <Modal.Header className="pb-0 border-0" closeButton />
       <Modal.Body className="p-0">{children}</Modal.Body>
     </Modal>
