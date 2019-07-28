9fb4c02c0eaac301fdcdf8191823c30d4b016f70
RukkiesMan <pavelekseev@gmail.com>
2019-07-28 10:31:24 +0300

    Move filter panel to standalone file

--- src/screens/Supplier/FilteredList/FilteredList.jsx
+++ src/screens/Supplier/FilteredList/FilteredList.jsx
@@ -4,13 +4,11 @@ import {
   Row,
   Container,
   Col,
-  Form,
   Button,
   ButtonToolbar,
   Card,
 } from 'react-bootstrap';
 import Pagination from 'react-js-pagination';
-import { Range } from 'rc-slider';
 
 import './FilteredList.scss';
 import 'rc-slider/assets/index.css';
@@ -22,6 +20,7 @@ import config from '../../../config';
 
 import ScreensLayoutMain from '../../Layouts/Main';
 import SearchPanel from '../../../components/SearchPanel';
+import FilterPanel from '../../../components/FilterPanel';
 
 const ScreensSupplierFilteredList = ({ history, match }) => {
   const [providers, setProviders] = useState([]);
@@ -63,7 +62,7 @@ const ScreensSupplierFilteredList = ({ history, match }) => {
       <Container className="supplier-list-results">
         <Row>
           <Col sm={4} className="results-filters">
-            <Filters setFilterOptions={setFilterOptions} />
+            <FilterPanel setFilterOptions={setFilterOptions} />
           </Col>
           <Col sm={8} className="results-data">
             <Providers
@@ -79,182 +78,6 @@ const ScreensSupplierFilteredList = ({ history, match }) => {
     </ScreensLayoutMain>
   );
 };
-
-function Filters({ setFilterOptions }) {
-  const budgetDefaultValues = [500, 1000];
-  const [budgetValues, setBudgetValues] = useState([...budgetDefaultValues]);
-
-  const guestsDefaultNumber = [30, 100];
-  const [guestsNumber, setGuestsNumber] = useState([...guestsDefaultNumber]);
-
-  const defaultVenueTypes = {
-    'Country House': false,
-    Barn: false,
-    Outdoor: false,
-    Attraction: false,
-  };
-  const [venueTypes, setVenueTypes] = useState(defaultVenueTypes);
-  const handleVenueTypeSelection = e => {
-    const venueName = e.target.id;
-    setVenueTypes({
-      ...venueTypes,
-      [venueName]: !venueTypes[venueName],
-    });
-  };
-
-  const defaultVenueStyles = {
-    Classic: false,
-    Intimate: false,
-    Unusual: false,
-    Modern: false,
-  };
-  const [venueStyles, setVenueStyles] = useState(defaultVenueStyles);
-  const handleVenueStyleSelection = e => {
-    const venueStyle = e.target.id;
-    setVenueStyles({
-      ...venueStyles,
-      [venueStyle]: !venueStyles[venueStyle],
-    });
-  };
-
-  return (
-    <React.Fragment>
-      <Form>
-        <div className="mb-5">
-          <div className="mb-3">
-            <b>Budget</b>
-          </div>
-          <Row>
-            <Col sm={12}>
-              <Range
-                min={0}
-                max={10000}
-                defaultValue={[...budgetDefaultValues]}
-                value={budgetValues}
-                step={10}
-                onChange={updatedValues => setBudgetValues(updatedValues)}
-              />
-            </Col>
-            <Col sm={12} className="mt-3">
-              <span className="text-muted">Price: </span>
-              {`$${budgetValues[0]} - $${budgetValues[1]}`}
-            </Col>
-          </Row>
-        </div>
-        <div className="mb-5">
-          <div className="mb-3">
-            <b>Seated Dining Capacity</b>
-          </div>
-          <Row>
-            <Col sm={12}>
-              <Range
-                min={0}
-                max={200}
-                defaultValue={[...guestsDefaultNumber]}
-                value={guestsNumber}
-                step={1}
-                onChange={updatedGuestsNumber =>
-                  setGuestsNumber(updatedGuestsNumber)
-                }
-              />
-            </Col>
-            <Col sm={12} className="mt-3">
-              <span className="text-muted">Seated: </span>
-              {`${guestsNumber[0]} - ${guestsNumber[1]}`}
-            </Col>
-          </Row>
-        </div>
-        <div className="mb-5">
-          <div className="mb-3">
-            <b>Venue Type</b>
-          </div>
-          <Form.Check
-            label="Country House"
-            id="Country House"
-            onChange={handleVenueTypeSelection}
-            checked={venueTypes['Country House']}
-          />
-          <Form.Check
-            label="Barn"
-            id="Barn"
-            onChange={handleVenueTypeSelection}
-            checked={venueTypes['Barn']}
-          />
-          <Form.Check
-            label="Outdoor"
-            id="Outdoor"
-            onChange={handleVenueTypeSelection}
-            checked={venueTypes['Outdoor']}
-          />
-          <Form.Check
-            label="Attraction"
-            id="Attraction"
-            onChange={handleVenueTypeSelection}
-            checked={venueTypes['Attraction']}
-          />
-        </div>
-        <div className="mb-5">
-          <div className="mb-3">
-            <b>Venue Styles</b>
-          </div>
-          <Form.Check
-            label="Classic"
-            id="Classic"
-            onChange={handleVenueStyleSelection}
-            checked={venueStyles['Classic']}
-          />
-          <Form.Check
-            label="Intimate"
-            id="Intimate"
-            onChange={handleVenueStyleSelection}
-            checked={venueStyles['Intimate']}
-          />
-          <Form.Check
-            label="Unusual"
-            id="Unusual"
-            onChange={handleVenueStyleSelection}
-            checked={venueStyles['Unusual']}
-          />
-          <Form.Check
-            label="Modern"
-            id="Modern"
-            onChange={handleVenueStyleSelection}
-            checked={venueStyles['Modern']}
-          />
-        </div>
-      </Form>
-      <ButtonToolbar>
-        <Button
-          variant="primary"
-          className="mr-2"
-          onClick={() => {
-            const filterOptionsObj = {
-              budgetValues,
-              guestsNumber,
-              venueTypes,
-              venueStyles,
-            };
-            console.log(filterOptionsObj);
-            setFilterOptions(filterOptionsObj);
-          }}
-        >
-          Apply Filter
-        </Button>
-        <Button
-          variant="dark"
-          onClick={() => {
-            setBudgetValues(budgetDefaultValues);
-            setGuestsNumber(guestsDefaultNumber);
-            setVenueTypes(defaultVenueTypes);
-            setVenueStyles(defaultVenueStyles);
-          }}
-        >
-          Clear
-        </Button>
-      </ButtonToolbar>
-    </React.Fragment>
-  );
-}
 
 function Providers({
   providers,
