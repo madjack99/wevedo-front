da38badd286541f7b2021a3d17e6c57d69919c86
RukkiesMan <pavelekseev@gmail.com>
2019-07-28 12:16:08 +0300

    Fix view of search panel on home screen

--- src/components/Search/Panel/Panel.jsx
+++ src/components/Search/Panel/Panel.jsx
@@ -3,21 +3,28 @@ import { withTranslation } from 'react-i18next';
 
 import { Row, Col, Form, Button } from 'react-bootstrap';
 
-const SearchPanel = ({ t }) => (
-  <Row className="findseparator findseparator-sm align-items-center text-center d-none d-sm-flex mb-5">
+const SearchPanel = ({ title, t }) => (
+  <Row
+    className={
+      title
+        ? 'findseparator d-flex align-items-center text-center'
+        : 'findseparator findseparator-sm align-items-center text-center d-none d-sm-flex mb-5'
+    }
+  >
     <Col sm={12}>
+      {title ? <h1>Find your best wedding supplier by Location</h1> : null}
       <Form>
         <Form.Row>
           <Col className="boxed-form">
             <Row>
-              <Col>
+              <Col sm={6} md>
                 <Form.Control as="select">
                   <option>{t('home.findForm.category')}</option>
                   <option>{t('home.findForm.options')}</option>
                 </Form.Control>
               </Col>
-              <div className="divider" />
-              <Col>
+              <div className="divider d-none d-sm-none d-md-inline" />
+              <Col sm={6} md>
                 <Form.Control
                   placeholder={t('home.findForm.locationPlaceholder')}
                 />
