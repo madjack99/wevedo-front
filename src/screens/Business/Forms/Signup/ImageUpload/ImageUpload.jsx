757a050e64fb17568da442b9d9d81aae6d3eefcd
RukkiesMan <pavelekseev@gmail.com>
2019-07-29 11:30:57 +0300

    Fix error with absense of history object

--- src/screens/Business/Forms/Signup/ImageUpload/ImageUpload.jsx
+++ src/screens/Business/Forms/Signup/ImageUpload/ImageUpload.jsx
@@ -4,9 +4,9 @@ import { withTranslation } from 'react-i18next';
 import ScreensLayoutsMinimalistForm from '../../../../Layouts/MinimalistForm';
 import ImageUpload from '../../../../../components/ImageUpload';
 
-const ScreensBusinessFormsSignupImageUpload = ({ t }) => (
+const ScreensBusinessFormsSignupImageUpload = ({ history, t }) => (
   <ScreensLayoutsMinimalistForm title={`${t('imgUpload.uploadPhotos')}`}>
-    <ImageUpload />
+    <ImageUpload history={history} />
   </ScreensLayoutsMinimalistForm>
 );
 
