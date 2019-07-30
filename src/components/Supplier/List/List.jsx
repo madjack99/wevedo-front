825ec49afc13256e7b678892c79c7ba0daac4648
madjack <madjack@nm.ru>
2019-07-30 13:30:14 +0400

    add cloudinary config to config

--- src/components/Supplier/List/List.jsx
+++ src/components/Supplier/List/List.jsx
@@ -28,6 +28,7 @@ const SupplierList = ({
 
   function SupplierGrid({ provider }) {
     const { _id: providerId } = provider;
+    console.log(provider);
     return (
       <Link to={`/suppliers/details/${providerId}`}>
         <Card className="h-100">
