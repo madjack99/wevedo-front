12f7ea361288df4a57e5a4bacec9f9fc6d8eda32
RukkiesMan <pavelekseev@gmail.com>
2019-08-03 09:07:20 +0300

    Add mobile header view

--- src/components/Dashboard/Header/Header.jsx
+++ src/components/Dashboard/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { Navbar } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 import logo from '../../../assets/images/symbol.png';
@@ -9,17 +9,54 @@ import defaultAvatar from '../../../assets/images/default-avatar.png';
 
 const Header = ({ user }) => {
   return (
-    <Navbar fixed="top" bg="light" variant="light" expand="lg">
-      <Navbar.Toggle />
-      <Navbar.Collapse>
+    <React.Fragment>
+      <Navbar
+        className="dashboard-header d-none d-sm-flex"
+        fixed="top"
+        bg="light"
+        variant="light"
+        expand="lg"
+      >
         <ProfileArea user={user} />
-      </Navbar.Collapse>
-      <Navbar.Brand>
-        <Link to="/">
-          <img src={logo} alt="logo" />
-        </Link>
-      </Navbar.Brand>
-    </Navbar>
+        <Navbar.Brand>
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+        </Navbar.Brand>
+      </Navbar>
+
+      {/* Mobile header */}
+      <Navbar
+        fixed="top"
+        bg="light"
+        variant="light"
+        expand="lg"
+        className="d-flex d-sm-none"
+      >
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav className="text-uppercase mr-auto">
+            <Nav.Link href="/dashboard/userhome">
+              <b>Home</b>
+            </Nav.Link>
+            <Nav.Link href="/dashboard/messages">
+              <b>Messages</b>
+            </Nav.Link>
+            <Nav.Link href="/dashboard/businessaccount">
+              <b>Account Settings</b>
+            </Nav.Link>
+            <Nav.Link href="/">
+              <b>Log Out</b>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <Navbar.Brand>
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+        </Navbar.Brand>
+      </Navbar>
+    </React.Fragment>
   );
 };
 
