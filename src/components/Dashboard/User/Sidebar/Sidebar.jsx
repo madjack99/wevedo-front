7af10440f8e1d091825936d154a227fbe400dfb1
RukkiesMan <pavelekseev@gmail.com>
2019-08-03 09:07:51 +0300

    Add dashboard user sidebar

---
+++ src/components/Dashboard/User/Sidebar/Sidebar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => (
+  <div className="sidebar d-none d-sm-block">
+    <div className="active">
+      <Link to="/dashboard/user">
+        <i className="fas fa-home" />
+      </Link>
+    </div>
+    <div>
+      <Link to="/">
+        <i className="far fa-comment-alt" />
+      </Link>
+    </div>
+    <div>
+      <Link to="/">
+        <i className="fas fa-scroll" />
+      </Link>
+    </div>
+    <div>
+      <Link to="/">
+        <i className="fas fa-calculator" />
+      </Link>
+    </div>
+    <div>
+      <Link to="/">
+        <i className="fas fa-power-off" />
+      </Link>
+    </div>
+  </div>
+);
+
+export default Sidebar;
