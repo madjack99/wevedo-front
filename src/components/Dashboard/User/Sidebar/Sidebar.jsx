85845561b249f2d0cfd432e1a7bf91194bd9fc79
RukkiesMan <pavelekseev@gmail.com>
2019-08-03 12:22:21 +0300

    Add sign out by clicking sign out button on sidebar

--- src/components/Dashboard/User/Sidebar/Sidebar.jsx
+++ src/components/Dashboard/User/Sidebar/Sidebar.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { connect } from 'react-redux';
+
 import { Link } from 'react-router-dom';
 
-const DashboardUserSidebar = () => (
-  <div className="sidebar d-none d-sm-block">
-    <div className="active">
-      <Link to="/dashboard/user">
-        <i className="fas fa-home" />
-      </Link>
-    </div>
-    <div>
-      <Link to="/dashboard/user/messages">
-        <i className="far fa-comment-alt" />
-      </Link>
-    </div>
-    {/* <div>
+import { fetchSignOut } from '../../../../actions';
+import { WevedoServiceContext } from '../../../../contexts';
+
+const DashboardUserSidebar = ({ signOut }) => {
+  const wevedoService = useContext(WevedoServiceContext);
+
+  return (
+    <div className="sidebar d-none d-sm-block">
+      <div className="active">
+        <Link to="/dashboard/user">
+          <i className="fas fa-home" />
+        </Link>
+      </div>
+      <div>
+        <Link to="/dashboard/user/messages">
+          <i className="far fa-comment-alt" />
+        </Link>
+      </div>
+      {/* <div>
       <Link to="/">
         <i className="fas fa-scroll" />
       </Link>
@@ -23,12 +31,20 @@ const DashboardUserSidebar = () => (
         <i className="fas fa-calculator" />
       </Link>
     </div> */}
-    <div>
-      <Link to="/">
-        <i className="fas fa-power-off" />
-      </Link>
+      <div>
+        <Link to="/" onClick={() => signOut(wevedoService.signOut)}>
+          <i className="fas fa-power-off" />
+        </Link>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
-export default DashboardUserSidebar;
+const mapDispatchToProps = dispatch => ({
+  signOut: fetchSignOut(dispatch),
+});
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(DashboardUserSidebar);
