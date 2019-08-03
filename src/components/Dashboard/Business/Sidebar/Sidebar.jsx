66b07ba4a34336c5803471bb34cf074bc7386099
RukkiesMan <pavelekseev@gmail.com>
2019-08-03 11:51:48 +0300

    Make correct link to business messages

--- src/components/Dashboard/Business/Sidebar/Sidebar.jsx
+++ src/components/Dashboard/Business/Sidebar/Sidebar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const DashboardBusinessSidebar = () => (
   <div className="sidebar d-none d-sm-block">
     <div className="active">
-      <Link to="/dashboard/user/messages">
+      <Link to="/dashboard/business/messages">
         <i className="far fa-comment-alt" />
       </Link>
     </div>
