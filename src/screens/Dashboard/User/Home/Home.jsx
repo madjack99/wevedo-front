fd7b4e3d47b87d87fb87e031f0782e4367a95c22
RukkiesMan <pavelekseev@gmail.com>
2019-08-03 10:12:50 +0300

    Remove panel from layout

--- src/screens/Dashboard/User/Home/Home.jsx
+++ src/screens/Dashboard/User/Home/Home.jsx
@@ -6,109 +6,142 @@ import Sample from '../../../../assets/images/sample-dashboard.png';
 import Sample2 from '../../../../assets/images/sample-dashboard2.png';
 
 import ScreensLayoutsDashboardUser from '../../../Layouts/Dashboard/User';
+import SearchPanel from '../../../../components/Search/Panel';
+import SearchPanelMobile from '../../../../components/Search/Panel/Mobile';
 
 const ScreensDashboardUserHome = () => (
   <ScreensLayoutsDashboardUser>
-    <Container className="mb-5">
-      <Row className="mt-5">
-        <Col>
-          <h6 className="text-proxima-bold">
-            {' '}
-            Find And Book Your Dream Wedding Venue{' '}
-          </h6>
-        </Col>
-      </Row>
-      <Row className="h-scroll">
-        <Col xs={12} sm={4} className="h-scroll__item h-scroll__item-cardtype1">
-          <div className="dashboard-user__home-cardtype1">
-            <img src={Sample} alt="" />
-            <div className="p-4">
-              <h6 className="text-proxima-bold">Fulham Palace</h6>
-              <b className="text-muted">
-                <i className="fas fa-map-marker-alt text-wevedo" /> South London
-              </b>
+    <div className="dashboard dashboard-user__home">
+      <div className="dashboard-background" />
+      <Container>
+        <Row className="venues-filters d-flex d-sm-none pt-4 pb-4 mb-4">
+          <Col className="d-inline">
+            <SearchPanelMobile />
+          </Col>
+        </Row>
+      </Container>
+      <SearchPanel />
+      <Container className="mb-5">
+        <Row className="mt-5">
+          <Col>
+            <h6 className="text-proxima-bold">
+              {' '}
+              Find And Book Your Dream Wedding Venue{' '}
+            </h6>
+          </Col>
+        </Row>
+        <Row className="h-scroll">
+          <Col
+            xs={12}
+            sm={4}
+            className="h-scroll__item h-scroll__item-cardtype1"
+          >
+            <div className="dashboard-user__home-cardtype1">
+              <img src={Sample} alt="" />
+              <div className="p-4">
+                <h6 className="text-proxima-bold">Fulham Palace</h6>
+                <b className="text-muted">
+                  <i className="fas fa-map-marker-alt text-wevedo" /> South
+                  London
+                </b>
+              </div>
             </div>
-          </div>
-        </Col>
-        <Col xs={12} sm={4} className="h-scroll__item h-scroll__item-cardtype1">
-          <div className="dashboard-user__home-cardtype1">
-            <img src={Sample} alt="" />
-            <div className="p-4">
-              <h6 className="text-proxima-bold">Fulham Palace</h6>
-              <b className="text-muted">
-                <i className="fas fa-map-marker-alt text-wevedo" /> South London
-              </b>
+          </Col>
+          <Col
+            xs={12}
+            sm={4}
+            className="h-scroll__item h-scroll__item-cardtype1"
+          >
+            <div className="dashboard-user__home-cardtype1">
+              <img src={Sample} alt="" />
+              <div className="p-4">
+                <h6 className="text-proxima-bold">Fulham Palace</h6>
+                <b className="text-muted">
+                  <i className="fas fa-map-marker-alt text-wevedo" /> South
+                  London
+                </b>
+              </div>
             </div>
-          </div>
-        </Col>
-        <Col xs={12} sm={4} className="h-scroll__item h-scroll__item-cardtype1">
-          <div className="dashboard-user__home-cardtype1">
-            <img src={Sample} alt="" />
-            <div className="p-4">
-              <h6 className="text-proxima-bold">Fulham Palace</h6>
-              <b className="text-muted">
-                <i className="fas fa-map-marker-alt text-wevedo" /> South London
-              </b>
+          </Col>
+          <Col
+            xs={12}
+            sm={4}
+            className="h-scroll__item h-scroll__item-cardtype1"
+          >
+            <div className="dashboard-user__home-cardtype1">
+              <img src={Sample} alt="" />
+              <div className="p-4">
+                <h6 className="text-proxima-bold">Fulham Palace</h6>
+                <b className="text-muted">
+                  <i className="fas fa-map-marker-alt text-wevedo" /> South
+                  London
+                </b>
+              </div>
             </div>
-          </div>
-        </Col>
-        <Col xs={12} sm={4} className="h-scroll__item h-scroll__item-cardtype1">
-          <div className="dashboard-user__home-cardtype1">
-            <img src={Sample} alt="" />
-            <div className="p-4">
-              <h6 className="text-proxima-bold">Fulham Palace</h6>
-              <b className="text-muted">
-                <i className="fas fa-map-marker-alt text-wevedo" /> South London
-              </b>
+          </Col>
+          <Col
+            xs={12}
+            sm={4}
+            className="h-scroll__item h-scroll__item-cardtype1"
+          >
+            <div className="dashboard-user__home-cardtype1">
+              <img src={Sample} alt="" />
+              <div className="p-4">
+                <h6 className="text-proxima-bold">Fulham Palace</h6>
+                <b className="text-muted">
+                  <i className="fas fa-map-marker-alt text-wevedo" /> South
+                  London
+                </b>
+              </div>
             </div>
-          </div>
-        </Col>
-        <Col className="d-block d-sm-none text-center m-1">
-          <Button size="lg">Show More</Button>
-        </Col>
-      </Row>
+          </Col>
+          <Col className="d-block d-sm-none text-center m-1">
+            <Button size="lg">Show More</Button>
+          </Col>
+        </Row>
 
-      <Row className="mt-4 mt-sm-5">
-        <Col>
-          <h6 className="text-proxima-bold"> Wevedo Categories </h6>
-        </Col>
-      </Row>
-      <Row className="h-scroll">
-        <Col className="h-scroll__item h-scroll__item-cardtype2">
-          <div className="dashboard-user__home-cardtype2">
-            <img src={Sample2} alt="" />
-            <b>Music</b>
-          </div>
-        </Col>
-        <Col className="h-scroll__item h-scroll__item-cardtype2">
-          <div className="dashboard-user__home-cardtype2">
-            <img src={Sample2} alt="" />
-            <b>Music</b>
-          </div>
-        </Col>
-        <Col className="h-scroll__item h-scroll__item-cardtype2">
-          <div className="dashboard-user__home-cardtype2">
-            <img src={Sample2} alt="" />
-            <b>Music</b>
-          </div>
-        </Col>
-        <Col className="h-scroll__item h-scroll__item-cardtype2">
-          <div className="dashboard-user__home-cardtype2">
-            <img src={Sample2} alt="" />
-            <b>Music</b>
-          </div>
-        </Col>
-        <Col className="h-scroll__item h-scroll__item-cardtype2">
-          <div className="dashboard-user__home-cardtype2">
-            <img src={Sample2} alt="" />
-            <b>Music</b>
-          </div>
-        </Col>
-        <Col className="d-block d-sm-none text-center m-1">
-          <Button size="lg">Show More</Button>
-        </Col>
-      </Row>
-    </Container>
+        <Row className="mt-4 mt-sm-5">
+          <Col>
+            <h6 className="text-proxima-bold"> Wevedo Categories </h6>
+          </Col>
+        </Row>
+        <Row className="h-scroll">
+          <Col className="h-scroll__item h-scroll__item-cardtype2">
+            <div className="dashboard-user__home-cardtype2">
+              <img src={Sample2} alt="" />
+              <b>Music</b>
+            </div>
+          </Col>
+          <Col className="h-scroll__item h-scroll__item-cardtype2">
+            <div className="dashboard-user__home-cardtype2">
+              <img src={Sample2} alt="" />
+              <b>Music</b>
+            </div>
+          </Col>
+          <Col className="h-scroll__item h-scroll__item-cardtype2">
+            <div className="dashboard-user__home-cardtype2">
+              <img src={Sample2} alt="" />
+              <b>Music</b>
+            </div>
+          </Col>
+          <Col className="h-scroll__item h-scroll__item-cardtype2">
+            <div className="dashboard-user__home-cardtype2">
+              <img src={Sample2} alt="" />
+              <b>Music</b>
+            </div>
+          </Col>
+          <Col className="h-scroll__item h-scroll__item-cardtype2">
+            <div className="dashboard-user__home-cardtype2">
+              <img src={Sample2} alt="" />
+              <b>Music</b>
+            </div>
+          </Col>
+          <Col className="d-block d-sm-none text-center m-1">
+            <Button size="lg">Show More</Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   </ScreensLayoutsDashboardUser>
 );
 
