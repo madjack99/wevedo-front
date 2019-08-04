6083caef1f6f85610e03963d188b0e752f62b421
madjack <madjack@nm.ru>
2019-08-04 18:35:24 +0400

    Add eual

--- src/screens/TermsAndConditions/TermsAndConditions.jsx
+++ src/screens/TermsAndConditions/TermsAndConditions.jsx
@@ -5,6 +5,7 @@ import { withTranslation } from 'react-i18next';
 import backgroundImage from '../../assets/images/common-header.png';
 
 import ScreensLayoutsMain from '../Layouts/Main';
+import eula from './wevedo_eula.pdf';
 
 const TermsAndConditions = ({ t }) => (
   <ScreensLayoutsMain
@@ -12,7 +13,8 @@ const TermsAndConditions = ({ t }) => (
     backgroundImage={backgroundImage}
   >
     <Container className="mt-5 mb-5">
-      <Row className="justify-content-center mt-5 mb-5">
+      <embed src={eula} type="application/pdf" width="100%" height="500px" />
+      {/* <Row className="justify-content-center mt-5 mb-5">
         <Col sm={8} className="text-center mb-3">
           <h4 className="text-uppercase">{t('terms.mainTitle')}</h4>
           <hr />
@@ -33,7 +35,7 @@ const TermsAndConditions = ({ t }) => (
           <p>{t('terms.secondText.pOne')}</p>
           <p>{t('terms.secondText.pTwo')}</p>
         </Col>
-      </Row>
+      </Row> */}
     </Container>
   </ScreensLayoutsMain>
 );
