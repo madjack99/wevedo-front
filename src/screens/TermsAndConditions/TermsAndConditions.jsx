13ab2c66d6b1276204f8a1f47bfd3f5611082e3e
madjack <madjack@nm.ru>
2019-08-05 13:02:07 +0400

    Move eula to assets/files, remove old code in term

--- src/screens/TermsAndConditions/TermsAndConditions.jsx
+++ src/screens/TermsAndConditions/TermsAndConditions.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Row, Container, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import { withTranslation } from 'react-i18next';
 
 import backgroundImage from '../../assets/images/common-header.png';
 
 import ScreensLayoutsMain from '../Layouts/Main';
-import eula from './wevedo_eula.pdf';
+import eula from '../../assets/files/wevedo_eula.pdf';
 
 const TermsAndConditions = ({ t }) => (
   <ScreensLayoutsMain
@@ -14,28 +14,6 @@ const TermsAndConditions = ({ t }) => (
   >
     <Container className="mt-5 mb-5">
       <embed src={eula} type="application/pdf" width="100%" height="500px" />
-      {/* <Row className="justify-content-center mt-5 mb-5">
-        <Col sm={8} className="text-center mb-3">
-          <h4 className="text-uppercase">{t('terms.mainTitle')}</h4>
-          <hr />
-        </Col>
-      </Row>
-      <Row>
-        <Col sm={12} className="mb-5">
-          <h5 className="text-uppercase text-proxima-bold">
-            {t('terms.firstText.titleOne')}
-          </h5>
-          <p>{t('terms.firstText.pOne')}</p>
-          <p>{t('terms.firstText.pTwo')}</p>
-        </Col>
-        <Col sm={12} className="mb-5">
-          <h5 className="text-uppercase text-proxima-bold">
-            {t('terms.secondText.titleOne')}
-          </h5>
-          <p>{t('terms.secondText.pOne')}</p>
-          <p>{t('terms.secondText.pTwo')}</p>
-        </Col>
-      </Row> */}
     </Container>
   </ScreensLayoutsMain>
 );
