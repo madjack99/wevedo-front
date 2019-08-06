4ae38ffe838791dc1dfcb72f49e01b1843ea4d0f
RukkiesMan <pavelekseev@gmail.com>
2019-08-06 17:40:47 +0300

    Add Y-axis margins for chat items

--- src/components/Dashboard/Messages/Chat/Item/Recipient/Recipient.jsx
+++ src/components/Dashboard/Messages/Chat/Item/Recipient/Recipient.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DashboardMessagesChatItemRecipient = () => {
   return (
-    <div className="dashboard-business__messageBox-message p-4">
+    <div className="dashboard-business__messageBox-message my-3 p-4">
       <p className="d-flex">
         Hi, <br />
         we are interested
--- src/components/Dashboard/Messages/Chat/Item/Sender/Sender.jsx
+++ src/components/Dashboard/Messages/Chat/Item/Sender/Sender.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DashboardMessagesChatItemRecipient = () => {
   return (
-    <div className="dashboard-business__messageBox-message dashboard-business__messageBox-message-reciever p-4">
+    <div className="dashboard-business__messageBox-message dashboard-business__messageBox-message-reciever my-3 p-4">
       <p className="d-flex">Ok</p>
       <p className="text-right m-0">
         <small>2:16pm</small>
