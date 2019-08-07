ca1f2493422d846ff158392338afba7eddc5de36
madjack <madjack@nm.ru>
2019-08-07 19:34:51 +0400

    Retrieve updateProfile from context

--- src/components/Dashboard/Account/Account.jsx
+++ src/components/Dashboard/Account/Account.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { connect } from 'react-redux';
 import { Formik } from 'formik';
 
@@ -6,65 +6,74 @@ import { Container, Row, Col, Form, InputGroup, Button } from 'react-bootstrap';
 
 import addImage from '../../../assets/images/addimg.png';
 
+import { WevedoServiceContext } from '../../../contexts';
+
 import { updateUser } from '../../../actions/user-actions';
 
 import nameSchema from './nameSchema';
 
-const DashboardAccount = ({ user }) => (
-  <div className="dashboard">
-    <div className="dashboard-background" />
-    <Container className="dashboard-business__profile">
-      <h6 className="mb-3 mb-sm-5 pl-3 pl-sm-0 text-proxima-bold">
-        {' '}
-        Your Account{' '}
-      </h6>
-      <Row>
-        <Col>
-          <div className="dashboard-business__profile__whitebox">
-            <Row>
-              <Col sm={3}>
-                <div className="text-center">
-                  <img src={addImage} alt="" />
-                  <p className="mt-2">Upload Photo</p>
-                </div>
-              </Col>
-              <Col sm={9}>
-                <Col sm={12} className="mb-4">
-                  <NameChangeForm fullName={user.fullName} />
-                </Col>
-
-                <Col sm={12} className="mb-4">
-                  <p className="text-muted">E-Mail address</p>
-                  <InputGroup className="d-none d-sm-flex">
-                    <Form.Control size="lg" type="email" />
-                    <div className="input-group-append">
-                      <Button>Change the email</Button>
-                    </div>
-                  </InputGroup>
-                  <Form.Group className="d-block d-sm-none">
-                    <Form.Control size="lg" type="email" className="mb-2" />
-                    <Button size="lg">Change the email</Button>
-                  </Form.Group>
-                </Col>
-                <Col sm={12}>
-                  <div className="mb-3">
-                    <p className="text-muted mb-0">
-                      Want to change your password?
-                    </p>
-                    <b>You will recieve an email with intructions.</b>
+const DashboardAccount = ({ user, updateUser }) => {
+  const wevedoService = useContext(WevedoServiceContext);
+  return (
+    <div className="dashboard">
+      <div className="dashboard-background" />
+      <Container className="dashboard-business__profile">
+        <h6 className="mb-3 mb-sm-5 pl-3 pl-sm-0 text-proxima-bold">
+          {' '}
+          Your Account{' '}
+        </h6>
+        <Row>
+          <Col>
+            <div className="dashboard-business__profile__whitebox">
+              <Row>
+                <Col sm={3}>
+                  <div className="text-center">
+                    <img src={addImage} alt="" />
+                    <p className="mt-2">Upload Photo</p>
                   </div>
-                  <Button size="lg">Change password</Button>
                 </Col>
-              </Col>
-            </Row>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  </div>
-);
+                <Col sm={9}>
+                  <Col sm={12} className="mb-4">
+                    <NameChangeForm
+                      fullName={user.fullName}
+                      updateProfile={wevedoService.updateProfile}
+                      updateUser={updateUser}
+                    />
+                  </Col>
 
-const NameChangeForm = ({ fullName }) => {
+                  <Col sm={12} className="mb-4">
+                    <p className="text-muted">E-Mail address</p>
+                    <InputGroup className="d-none d-sm-flex">
+                      <Form.Control size="lg" type="email" />
+                      <div className="input-group-append">
+                        <Button>Change the email</Button>
+                      </div>
+                    </InputGroup>
+                    <Form.Group className="d-block d-sm-none">
+                      <Form.Control size="lg" type="email" className="mb-2" />
+                      <Button size="lg">Change the email</Button>
+                    </Form.Group>
+                  </Col>
+                  <Col sm={12}>
+                    <div className="mb-3">
+                      <p className="text-muted mb-0">
+                        Want to change your password?
+                      </p>
+                      <b>You will recieve an email with intructions.</b>
+                    </div>
+                    <Button size="lg">Change password</Button>
+                  </Col>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+const NameChangeForm = ({ fullName, updateProfile, updateUser }) => {
   return (
     <Formik
       className="form"
