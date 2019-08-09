dbd63855bd8cfe8caf3d76ecba26763bf48dcc2c
madjack <madjack@nm.ru>
2019-08-09 13:07:10 +0400

    Add default values for fields

--- src/components/Dashboard/Business/Profile/forms/basicInfoForm/BasicInfoForm.jsx
+++ src/components/Dashboard/Business/Profile/forms/basicInfoForm/BasicInfoForm.jsx
@@ -4,50 +4,61 @@ import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 const BasicInfoForm = () => {
   return (
-    <Formik>
-      {() => {
+    <Formik
+      className="form"
+      enableReinitialize
+      initialValues={{
+        bio: 'bio',
+        minPrice: 'minPrice',
+        maxPrice: 'maxPrice',
+        facilities: 'facilities',
+      }}
+    >
+      {({ values }) => {
         return (
-          <Row className="mb-5">
-            <Col>
-              <div className="dashboard-business__profile__whitebox">
-                <Col sm={12} className="mb-4">
-                  <Form.Group>
-                    <p className="text-muted">Description</p>
-                    <Form.Control as="textarea" rows="3" />
-                  </Form.Group>
-                </Col>
-                <Col sm={12} className="mb-4">
-                  <p className="text-muted">Pricing</p>
-                  <Row>
-                    <Col sm={4} className="mb-2 mb-sm-0">
-                      <Form.Control />
-                    </Col>
-                    <Col sm={4}>
-                      <Form.Control />
-                    </Col>
-                  </Row>
-                </Col>
-                <Col sm={12} className="mb-4">
-                  <p className="text-muted">Guests Capacity</p>
-                  <Row>
-                    <Col sm={4} className="mb-2 mb-sm-0">
-                      <Form.Control />
-                    </Col>
-                    <Col sm={4}>
-                      <Form.Control />
-                    </Col>
-                  </Row>
-                </Col>
-                <Col sm={12} className="mb-4">
-                  <p className="text-muted">Facilities</p>
-                  <Form.Control />
-                </Col>
-                <Col sm={12} className="text-uppercase mt-2 mb-4">
-                  <Button size="lg">Save</Button>
-                </Col>
-              </div>
-            </Col>
-          </Row>
+          <Form>
+            <Row className="mb-5">
+              <Col>
+                <div className="dashboard-business__profile__whitebox">
+                  <Col sm={12} className="mb-4">
+                    <Form.Group>
+                      <p className="text-muted">Description</p>
+                      <Form.Control as="textarea" rows="3" value={values.bio} />
+                    </Form.Group>
+                  </Col>
+                  <Col sm={12} className="mb-4">
+                    <p className="text-muted">Pricing</p>
+                    <Row>
+                      <Col sm={4} className="mb-2 mb-sm-0">
+                        <Form.Control value={values.minPrice} />
+                      </Col>
+                      <Col sm={4}>
+                        <Form.Control value={values.minPrice} />
+                      </Col>
+                    </Row>
+                  </Col>
+                  <Col sm={12} className="mb-4">
+                    <p className="text-muted">Guests Capacity</p>
+                    <Row>
+                      <Col sm={4} className="mb-2 mb-sm-0">
+                        <Form.Control />
+                      </Col>
+                      <Col sm={4}>
+                        <Form.Control />
+                      </Col>
+                    </Row>
+                  </Col>
+                  <Col sm={12} className="mb-4">
+                    <p className="text-muted">Facilities</p>
+                    <Form.Control value={values.facilities} />
+                  </Col>
+                  <Col sm={12} className="text-uppercase mt-2 mb-4">
+                    <Button size="lg">Save</Button>
+                  </Col>
+                </div>
+              </Col>
+            </Row>
+          </Form>
         );
       }}
     </Formik>
