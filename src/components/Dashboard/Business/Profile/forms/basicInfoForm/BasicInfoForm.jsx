9ab2adf4ce06571847782153103f45ce88fd2b6c
madjack <madjack@nm.ru>
2019-08-09 13:30:51 +0400

    Add name and handleChange handlers

--- src/components/Dashboard/Business/Profile/forms/basicInfoForm/BasicInfoForm.jsx
+++ src/components/Dashboard/Business/Profile/forms/basicInfoForm/BasicInfoForm.jsx
@@ -14,7 +14,7 @@ const BasicInfoForm = ({ user }) => {
         facilities: user.facilities || 'facilities',
       }}
     >
-      {({ values }) => {
+      {({ values, handleChange }) => {
         return (
           <Form>
             <Row className="mb-5">
@@ -26,8 +26,10 @@ const BasicInfoForm = ({ user }) => {
                       <Form.Control
                         as="textarea"
                         rows="3"
+                        name="bio"
                         value={values.bio}
                         className=" form__control__account "
+                        onChange={handleChange}
                       />
                     </Form.Group>
                   </Col>
@@ -36,14 +38,18 @@ const BasicInfoForm = ({ user }) => {
                     <Row>
                       <Col sm={4} className="mb-2 mb-sm-0">
                         <Form.Control
+                          name="minPrice"
                           value={values.minPrice}
                           className=" form__control__account "
+                          onChange={handleChange}
                         />
                       </Col>
                       <Col sm={4}>
                         <Form.Control
-                          value={values.minPrice}
+                          name="maxPrice"
+                          value={values.maxPrice}
                           className=" form__control__account "
+                          onChange={handleChange}
                         />
                       </Col>
                     </Row>
@@ -52,18 +58,26 @@ const BasicInfoForm = ({ user }) => {
                     <p className="text-muted">Guests Capacity</p>
                     <Row>
                       <Col sm={4} className="mb-2 mb-sm-0">
-                        <Form.Control className=" form__control__account " />
+                        <Form.Control
+                          className=" form__control__account "
+                          onChange={handleChange}
+                        />
                       </Col>
                       <Col sm={4}>
-                        <Form.Control className=" form__control__account " />
+                        <Form.Control
+                          className=" form__control__account "
+                          onChange={handleChange}
+                        />
                       </Col>
                     </Row>
                   </Col>
                   <Col sm={12} className="mb-4">
                     <p className="text-muted">Facilities</p>
                     <Form.Control
+                      name="facilities"
                       value={values.facilities}
                       className=" form__control__account "
+                      onChange={handleChange}
                     />
                   </Col>
                   <Col sm={12} className="text-uppercase mt-2 mb-4">
