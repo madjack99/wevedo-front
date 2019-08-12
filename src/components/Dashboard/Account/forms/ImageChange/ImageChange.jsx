1ee0997d9420e0d384f2f260e998e61903404ce1
madjack <madjack@nm.ru>
2019-08-12 18:16:36 +0400

    Do additional styling for profile image

--- src/components/Dashboard/Account/forms/ImageChange/ImageChange.jsx
+++ src/components/Dashboard/Account/forms/ImageChange/ImageChange.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Dropzone from 'react-dropzone';
-import { Col, Container } from 'react-bootstrap';
+import { Col, Image } from 'react-bootstrap';
 import addImage from '../../../../../assets/images/addimg.png';
 
 const DashboardAccountFormsImageChange = ({
@@ -17,29 +17,33 @@ const DashboardAccountFormsImageChange = ({
 
     try {
       const { data: cloudinaryPhotos } = await loadImagesToServer(formData);
-      console.log(cloudinaryPhotos);
       const newProfileImageURL = cloudinaryPhotos[0].secure_url;
-      console.log(newProfileImageURL);
       updateUser(updateProfile)({ profileImageURL: newProfileImageURL });
     } catch (err) {
       console.log(err);
     }
   };
-  console.log('profileImageURL:', profileImageURL);
   return (
-    <Dropzone accept="image/*" onDrop={onDrop}>
-      {({ getRootProps, getInputProps }) => {
-        return (
-          <Col sm={3} {...getRootProps()}>
-            <input {...getInputProps()} />
-            <div className="text-center">
-              <img src={profileImageURL || addImage} alt="" />
-              <p className="mt-2">Upload Photo</p>
+    <Col sm={3}>
+      <Dropzone accept="image/*" onDrop={onDrop}>
+        {({ getRootProps, getInputProps }) => {
+          return (
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              <div className="text-center">
+                <Image
+                  roundedCircle
+                  style={{ width: 150, height: 150 }}
+                  src={profileImageURL || addImage}
+                  alt="profile image"
+                />
+                <p className="mt-2">Upload Photo</p>
+              </div>
             </div>
-          </Col>
-        );
-      }}
-    </Dropzone>
+          );
+        }}
+      </Dropzone>
+    </Col>
   );
 };
 
