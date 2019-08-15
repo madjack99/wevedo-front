05c465a07fabe1915244a369e01988bd926cb8c6
Pavel Alekseev <pavelekseev@gmail.com>
2019-08-15 09:58:38 +0300

    Rename schemas to schema to follow common project convention

--- src/components/Dashboard/Account/forms/EmailChange/schema.js
+++ src/components/Dashboard/Account/forms/EmailChange/schema.js
@@ -4,4 +4,8 @@ export default Yup.object().shape({
   email: Yup.string()
     .email('enter valid email')
     .required('email is required'),
+  password: Yup.string()
+    .min(6, 'at least 6 symbols')
+    .max(50, 'maximum 50 characters')
+    .required('password is required!'),
 });
