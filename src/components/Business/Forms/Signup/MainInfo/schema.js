c6f745d24f7c99b863e82b946b157a5a31e649c9
Pavel Alekseev <pavelekseev@gmail.com>
2019-08-16 10:52:59 +0300

    Fix error textes

--- src/components/Business/Forms/Signup/MainInfo/schema.js
+++ src/components/Business/Forms/Signup/MainInfo/schema.js
@@ -2,18 +2,18 @@ import * as Yup from 'yup';
 
 export default Yup.object().shape({
   username: Yup.string()
-    .min(6, 'at least 6 symbols')
+    .min(6, 'minimum 6 characters')
     .max(50, 'maximum 50 characters')
     .required('username is required'),
   password: Yup.string()
-    .min(6, 'at least 6 symbols')
+    .min(6, 'minimum 6 characters')
     .max(50, 'maximum 50 characters')
     .required('password is required!'),
   confirmPassword: Yup.string()
     .oneOf([Yup.ref('password'), null], 'passwords must match')
     .required('confirm password is required'),
   fullName: Yup.string()
-    .min(6, 'at least 6 symbols')
+    .min(6, 'minimum 6 characters')
     .max(50, 'maximum 50 characters')
     .required('name is required'),
   categories: Yup.string().required('category is required'),
