ce96133a62f2093b241370aabd13827b89d595f7
madjack <madjack@nm.ru>
2019-09-11 18:01:27 +0400

    Make helper function isUrl

--- src/screens/Supplier/Details/Details.jsx
+++ src/screens/Supplier/Details/Details.jsx
@@ -21,6 +21,7 @@ import PopularSearches from '../../../components/PopularSearches';
 import PromotedProviders from '../../../components/PromotedSuppliers';
 import Footer from '../../../components/Footer';
 import MyCalendar from '../../../components/Calendar';
+import { isUrl } from '../../../helpers';
 
 const SupplierDetails = ({ isLoggedIn, user, match, t, history }) => {
   const [supplier, setSupplier] = useState({});
@@ -44,21 +45,6 @@ const SupplierDetails = ({ isLoggedIn, user, match, t, history }) => {
     : [];
 
   const bookedDates = supplier.bookedDates ? supplier.bookedDates : [];
-
-  function isUrl(str) {
-    const regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
-    if (regexp.test(str)) {
-      if (!str.includes('http')) {
-        str = `http://${supplier.website}`;
-      }
-      return (
-        <a href={str}>
-          <b>{` ${supplier.website}`}</b>
-        </a>
-      );
-    }
-    return <b>{` ${supplier.website}`}</b>;
-  }
 
   return (
     <React.Fragment>
