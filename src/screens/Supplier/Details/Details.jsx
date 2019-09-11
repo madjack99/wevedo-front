3e78f116e92b286451d99539f87293c3f5b830a1
madjack <madjack@nm.ru>
2019-09-11 13:54:05 +0400

    Add regex for website link rendering

--- src/screens/Supplier/Details/Details.jsx
+++ src/screens/Supplier/Details/Details.jsx
@@ -45,6 +45,21 @@ const SupplierDetails = ({ isLoggedIn, user, match, t, history }) => {
 
   const bookedDates = supplier.bookedDates ? supplier.bookedDates : [];
 
+  function isUrl(str) {
+    const regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    if (regexp.test(str)) {
+      if (!str.includes('http')) {
+        str = `http://${supplier.website}`;
+      }
+      return (
+        <a href={str}>
+          <b>{` ${supplier.website}`}</b>
+        </a>
+      );
+    }
+    return <b>{` ${supplier.website}`}</b>;
+  }
+
   return (
     <React.Fragment>
       <Header />
@@ -91,9 +106,7 @@ const SupplierDetails = ({ isLoggedIn, user, match, t, history }) => {
             {supplier.website ? (
               <div className="d-block mb-4">
                 <b className="text-uppercase text-muted">Website:</b>
-                <a href={supplier.website}>
-                  <b>{` ${supplier.website}`}</b>
-                </a>
+                {isUrl(supplier.website)}
               </div>
             ) : null}
             {supplier.email ? (
